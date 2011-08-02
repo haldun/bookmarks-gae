e50f3204552a6e3a11a7ec3a30e1fddad135d7a8
Haldun Bayhantopcu <hb@haldun.me>
2011-08-02 22:55:14 +0300

    Fixed dependencies etc

--- link_libraries.py
+++ link_libraries.py
@@ -1,7 +1,19 @@
 import os
-
+import html5lib
 import tornado
-os.symlink(os.path.dirname(tornado.__file__), 'tornado')
-
 import wtforms
-os.symlink(os.path.dirname(wtforms.__file__), 'wtforms')
+
+try:
+  os.symlink(os.path.dirname(tornado.__file__), 'tornado')
+except:
+  pass
+
+try:
+  os.symlink(os.path.dirname(wtforms.__file__), 'wtforms')
+except:
+  pass
+
+try:
+  os.symlink(os.path.dirname(html5lib.__file__), 'html5lib')
+except:
+  pass
